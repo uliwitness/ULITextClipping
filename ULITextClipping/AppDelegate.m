fa8cb21ba795183910ef51942fbcc2706ebd156b
Uli Kusterer <ulivc@zathras.de>
2017-05-04 00:00:07 +0200

    Initial Commit

---
+++ ULITextClipping/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  ULITextClipping
+//
+//  Created by Uli Kusterer on 04.05.17.
+//  Copyright © 2017 Uli Kusterer. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+@end
